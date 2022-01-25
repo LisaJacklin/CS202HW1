e16bad8b05406883099ad6c7667953b9a5603592
Lisa Jacklin <lisaloveslola09@outlook.com>
2022-01-24 18:04:37 -0900

    TimeIt2 created

--- TimeIt1/TimeIt1.cpp
+++ TimeIt1/TimeIt1.cpp
@@ -4,6 +4,22 @@
 // 1/21/2022
 //
 
+/*
+Requirements:
+create StopWatch class,
+include a constructor to start the clock
+Start method to restart the clock,
+Stop method to record the end time for an event.
+Include two different methods to report the time in seconds and milliseconds as doubles
+use chrono header from STL to get time from system.
+
+use this to test search, binary_search, and two othe ralgorithms in STLa
+measure time for each algorithm at least 5 times in ranges:
+10,100,1K,10K, 100K, 1M, 10M, 100M, 1B
+
+*/
+
+
 #include <iostream>
 
 int main()
---
+++ TimeIt2/TimeIt2.cpp
@@ -0,0 +1,12 @@
+// TimeIt2.cpp
+//Lisa Jacklin
+//CS 202
+//1/21/2022
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+

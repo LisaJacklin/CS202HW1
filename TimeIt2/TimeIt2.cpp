6229f8a3941bbbf29ccc567596730dde678595fd
Lisa Jacklin <lisaloveslola09@outlook.com>
2022-01-24 18:09:12 -0900

    c_style_strings created

--- TimeIt2/TimeIt2.cpp
+++ TimeIt2/TimeIt2.cpp
@@ -3,6 +3,18 @@
 //CS 202
 //1/21/2022
 
+/*
+Requirements:
+use StopWatch class to time the same algorithms but with 
+reading at least 5 gutenberg project books w/ std::string.
+
+time how long it takes w three kinds of containers: list, vector, etc.
+time how long to find a random string inside each containers.
+
+take the output and graph it in microsoft excel.
+
+*/
+
 #include <iostream>
 
 int main()
---
+++ c_style_strings/c_style_strings.cpp
@@ -0,0 +1,11 @@
+// c_style_strings.cpp 
+//Lisa Jacklin
+//CS 202
+//1/21/2022
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}

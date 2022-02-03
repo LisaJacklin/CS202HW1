63c95634816425971f609557c7f2e8b8ff2e0ae2
Lisa Jacklin <lisaloveslola09@outlook.com>
2022-02-02 23:41:16 -0900

    timeIt1.cpp random number generator added

--- TimeIt1/TimeIt1.cpp
+++ TimeIt1/TimeIt1.cpp
@@ -15,26 +15,25 @@ using std::endl;
 Requirements:
 
 use this to test search, binary_search, 
-and two othe ralgorithms in STLa
+and two other algorithms in STLa
 measure time for each algorithm at least 5 times in ranges:
 10,100,1K,10K, 100K, 1M, 10M, 100M, 1B
 
 */
 
+//note sure where this belongs...
+//random number generator
+//this is used to generate a certain amount of random numbers for which
+//the program has to search. 
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> dis(0);
+
+
 
 int main()
 {
-    //include a constructor to start the clock
-    //include start and stop method
-
-    //note sure where this belongs...
-    //generates a random number generator
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0);
-
+  
 
 
 }
-
-//What can you say about the comparative asymptotic performance for those algorithms?
